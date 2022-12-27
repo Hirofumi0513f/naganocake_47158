7fa631dd04e8ab82222dc081106e0818ff713762
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2022-12-27 10:43:50 +0000

    [Update]naganocake_2022-1227-1943

--- config/routes.rb
+++ config/routes.rb
@@ -1,50 +1,13 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-    get 'addresses/create'
-    get 'addresses/update'
-    get 'addresses/destroy'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/complete'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
-    get 'cart_items/destroy_all'
-    get 'cart_items/create'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/unsubscribe'
-    get 'customers/withdrawal'
-  end
-  namespace :public do
-    get 'sessions/new'
-    get 'sessions/create'
-    get 'sessions/destroy'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  namespace :public do
-    get 'registrations/new'
-    get 'registrations/create'
-  end
+  resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
+  resources :customers, only: [:show, :edit, :update, :unsubscribe, :withdrawal]
+  resources :homes, only: [:top, :about]
+  resources :items, only: [:index, :show]
+  resources :orders, only: [:new, :confirm, :complete, :create, :index, :show]
+  resources :registrations, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+
   namespace :admin do
     resources :customers, only: [:index, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
@@ -57,5 +20,5 @@ Rails.application.routes.draw do
   end
 
 
-  
+
 end
