9c9570a5044843cd5e4dd4bf831003863ee460c9
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2022-12-27 23:22:56 +0000

    [Update]naganocake_2022-1228-0822

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,24 @@
 Rails.application.routes.draw do
   root to: "public/homes#top"
   get '/about', to: "public/homes#about", as: "about"
+
   resources :addresses, only: [:index, :edit, :create, :update, :destroy]
-  resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
+
+  resources :cart_items, only: [:index, :update, :destroy, :create]
+  delete '/cart_items/destroy_all',to:"cart_items#destroy_all", as: "destroy_all"
+
   resources :customers, only: [:show, :edit, :update, :unsubscribe, :withdrawal]
+  get '/customers/unsubscribe',to:"customers#unsubscribe", as: "unsubscribe"
+  patch '/customers/withdrawal',to:"customers#withdrawal", as: "withdrawal"
+
   resources :items, only: [:index, :show]
-  resources :orders, only: [:new, :confirm, :complete, :create, :index, :show]
+
+  resources :orders, only: [:new, :create, :index, :show]
+  post '/orders/confilm' , to: "orders#confilm", as: "confilm"
+  get  '/orders/complete', to: "orders#complete", as: "complete"
+
   resources :registrations, only: [:new, :create]
+
   resources :sessions, only: [:new, :create, :destroy]
 
   devise_for:customers,skip: [:passwords],controllers:{
