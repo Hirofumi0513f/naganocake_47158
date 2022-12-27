aba79f4ce7cbad159da07151bfbf759f83d4230b
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2022-12-27 11:49:36 +0000

    [Update]naganocake_2022-1227-2049

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,22 @@
 Rails.application.routes.draw do
+  root to: "public/homes#top"
+  get '/home/about', to: "public/homes#about", as: "about"
   resources :addresses, only: [:index, :edit, :create, :update, :destroy]
   resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
   resources :customers, only: [:show, :edit, :update, :unsubscribe, :withdrawal]
-  resources :homes, only: [:top, :about]
   resources :items, only: [:index, :show]
   resources :orders, only: [:new, :confirm, :complete, :create, :index, :show]
   resources :registrations, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
   namespace :admin do
+    root to: "homes#top"
     resources :customers, only: [:index, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
-    resources :homes, only: [:top]
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :order_details, only: [:update]
     resources :orders, only: [:show, :update]
     resources :sessions, only: [:new, :create, :destroy]
-      devise_for :admins
   end
-
-
 
 end
