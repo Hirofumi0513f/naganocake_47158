d6b4ea5f89f5f2a08ce4a7bb8b1258b65b0274d6
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2023-01-01 07:57:34 +0000

    [Updata]naganocake_2023-0101-1657

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -10,4 +10,10 @@ class Admin::GenresController < ApplicationController
 
   def update
   end
+
+  private
+  #商品画面に画像を追加する機能を追加するために記載。2023/1/1
+  def genres_params
+    params.require(:admin).permit(:image)
+  end
 end
