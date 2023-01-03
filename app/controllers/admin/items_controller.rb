6d9bdfebb24e7af362663cca19a76bc1b24c6a71
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2023-01-03 09:15:15 +0000

    [Update]naganocake_2023-0103-1815

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ItemsController < ApplicationController
-  before_action :authenticate_admin!
+  # ↓ログイン済ユーザーのみにアクセスを許可する
+  # before_action :authenticate_admin!
   def index
     # kaminariを用いてページネーションを実装。商品id順に1ページあたり10件表示されるようにする。
     @items = Item.order(:id).page(params[:page]).per(10)
@@ -10,7 +11,7 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new
+    @item = Item.new(item_params)
     @item.genre.id = genre.id
     @item.save
     redirect_to admin_items_path
@@ -24,5 +25,12 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
+  end
+
+  # 以下ストロングパラメータ
+  private
+
+  def item_params
+    params.require(:item).permit(:item_photo, :name, :introduction, :genre_id, :price, :is_active)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,10 @@
 class Item < ApplicationRecord
+  # itemモデルにimageを持たせる
+  has_one_attached :image
+  
   enum is_active: { on_sale: 0, sold_out: 1}
 
   belongs_to :genre
-  has_many   :cart_items  , dependent: :destory
-  has_many   :order_detils, dependent: :destory
+  # has_many   :cart_items  , dependent: :destory
+  # has_many   :order_detils, dependent: :destory
 end
