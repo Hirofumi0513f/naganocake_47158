b174f1d2af10143e7309a382a2e53add942bd922
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2023-01-05 13:23:21 +0000

    [Update]naganocake_2023-0105-2223

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,39 +2,43 @@ class Admin::ItemsController < ApplicationController
   # ↓管理者ユーザーのみにアクセスを許可する
   before_action :authenticate_admin!
 
-  def index
-    # kaminariを用いてページネーションを実装。商品id順に1ページあたり10件表示されるようにする。
-    @items = Item.order(:id).page(params[:page]).per(10)
-  end
-
-  def new
-    @item = Item.new
-  end
-
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      flash[:notice] = "Item was successfully created."
-      redirect_to admin_items_path
-  end
-
-  def show
-    @itemf = Item.find(params[:id])
-    @genre = @item.genres
-    @item = Item.new
-  end
-
-  def edit
-    @itemf = Item.find(params[:id])
-  end
-
-  def update
-  end
-
-  # 以下ストロングパラメータ
-  private
-    def item_params
-      params.require(:item).permit(:item_photo, :name, :introduction, :genre_id, :price, :is_active)
+    def index
+      # kaminariを用いてページネーションを実装。商品id順に1ページあたり10件表示されるようにする。
+      @items = Item.order(:id).page(params[:page]).per(10)
+      @item = Item.new
     end
-  end
+
+    def new
+      @item = Item.new
+    end
+
+    def create
+      @item = Item.new(item_params)
+      if @item.save
+        flash[:notice] = "Item was successfully created."
+        redirect_to admin_items_path
+      else
+        @items = Item.all
+        render :index
+      end
+    end
+
+    def show
+      @itemf = Item.find(params[:id])
+      @genre = @item.genres
+      @item = Item.new
+    end
+
+    def edit
+      @itemf = Item.find(params[:id])
+    end
+
+    def update
+    end
+
+    # 以下ストロングパラメータ
+    private
+      def item_params
+        params.require(:item).permit(:item_photo, :name, :introduction, :genre_id, :price, :is_active)
+      end
 end
