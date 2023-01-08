1916297bb8591799b7592cdfb371dd68c45f4d63
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2023-01-08 13:10:41 +0000

    [Upadate]naganocake_2023-0108-2210

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -7,11 +7,30 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+    @customerf = Customer.find(params[:id])
   end
 
   def edit
+    @customerf = Customer.find(params[:id])
   end
 
   def update
+    @customerf = Customer.find(params[:id])
+    if @customerf.update(customer_params)
+      flash[:notice] = "Customer updated succssfully."
+      redirect_to admin_customer_path(@customerf.id)
+    else
+      render :edit
+    end
   end
+
+    # 以下ストロングパラメータ
+    # 意図しないデータの登録・更新を防ぐ機能
+    private
+      def customer_params
+        params.require(:customer).permit(
+          :last_name, :first_name, :last_name_kana, :first_name_kanam,
+          :postal_code, :address, :telephone_number, :email,
+          :is_deleted)
+      end
 end
