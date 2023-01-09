2446c7c4484a48483fb7b2b0f287581238c3f300
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2023-01-09 11:26:02 +0000

    [Update]naganocake_2023-0109-2025

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,29 +7,29 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-# seedファイルは一度実行するとDBの中に作成されるので、再度rails db:seedをする際は、記載した内容は削除しておくと良い
-Admin.create!(
-  email: 'test-admin@test.com',
-  password: 'test-admin')
+# seedファイルは一度実行するとDBの中に作成されるので、再度rails db:seedをする際は、記載した内容は削除するかコメントアウトすること
+# Admin.create!(
+  # email: 'test-admin@test.com',
+  # password: 'test-admin')
 
-Genre.create!(
-  [
-    {name: 'ケーキ'},
-    {name: 'プリン'},
-    {name: '焼き菓子'},
-    {name: 'キャンディ'}
-  ]
-  )
+# Genre.create!(
+  # [
+    # {name: 'ケーキ'},
+    # {name: 'プリン'},
+    # {name: '焼き菓子'},
+    # {name: 'キャンディ'}
+  # ]
+  # )
 
-  Customer.create!(
-    last_name: '福田',
-    first_name: '泰史',
-    last_name_kana: 'フクダ',
-    first_name_kana: 'ヒロフミ',
-    telephone_number: '000-1111-2222',
-    email: 'hirofumi_f@example.com',
-    password: '19930513',
-    postal_code: '000-1111',
-    address: '東京都葵市1−16−16 パークハイツ家康123',
-    is_deleted: 'FALSE'
-    )
+  # Customer.create!(
+    # last_name: '福田',
+    # first_name: '泰史',
+    # last_name_kana: 'フクダ',
+    # first_name_kana: 'ヒロフミ',
+    # telephone_number: '000-1111-2222',
+    # email: 'hirofumi_f@example.com',
+    # password: '19930513',
+    # postal_code: '000-1111',
+    # address: '東京都葵市1−16−16 パークハイツ家康123',
+    # is_deleted: 'FALSE'
+    # )
