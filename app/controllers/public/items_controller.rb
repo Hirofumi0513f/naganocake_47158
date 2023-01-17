60043b0c679e7b2fb1f3b1a39e9048ebd0913b9b
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2023-01-17 13:05:10 +0000

    [Update]naganocake_2023-0117-2205

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,5 +1,7 @@
 class Public::ItemsController < ApplicationController
   def index
+    # kaminariを用いてページネーションを実装。商品id順に1ページあたり8件表示されるようにする。
+    @items = Item.order(:id).page(params[:page]).per(8)
   end
 
   def show
