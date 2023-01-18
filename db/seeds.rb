6003e4e6b42bfd6150b1d4349331331b5bbad832
EC2 Default User <ec2-user@ip-172-31-0-111.ap-northeast-1.compute.internal>
2023-01-18 12:33:26 +0000

    [Update]naganocake_2023-0118-2133

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,27 +9,27 @@
 
 # seedファイルは一度実行するとDBの中に作成されるので、再度rails db:seedをする際は、記載した内容は削除するかコメントアウトすること
 # Admin.create!(
-  # email: 'test-admin@test.com',
-  # password: 'test-admin')
+#   email: 'test-admin@test.com',
+#   password: 'test-admin')
 
 # Genre.create!(
-  # [
-    # {name: 'ケーキ'},
-    # {name: 'プリン'},
-    # {name: '焼き菓子'},
-    # {name: 'キャンディ'}
-  # ]
-  # )
+#   [
+#     {name: 'ケーキ'},
+#     {name: 'プリン'},
+#     {name: '焼き菓子'},
+#     {name: 'キャンディ'}
+#   ]
+#   )
 
-  # Customer.create!(
-    # last_name: '福田',
-    # first_name: '泰史',
-    # last_name_kana: 'フクダ',
-    # first_name_kana: 'ヒロフミ',
-    # telephone_number: '000-1111-2222',
-    # email: 'hirofumi_f@example.com',
-    # password: '19930513',
-    # postal_code: '000-1111',
-    # address: '東京都葵市1−16−16 パークハイツ家康123',
-    # is_deleted: 'FALSE'
-    # )
+#   Customer.create!(
+#     last_name: '福田',
+#     first_name: '泰史',
+#     last_name_kana: 'フクダ',
+#     first_name_kana: 'ヒロフミ',
+#     telephone_number: '000-1111-2222',
+#     email: 'hirofumi_f@example.com',
+#     password: '19930513',
+#     postal_code: '000-1111',
+#     address: '東京都葵市1−16−16 パークハイツ家康123',
+#     is_deleted: 'FALSE'
+#     )
