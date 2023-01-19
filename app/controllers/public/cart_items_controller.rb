825843192c4847dc3c8ca3196d70b1a47e093df0
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2023-01-19 12:50:41 +0000

    [Update]naganocake_2023-0119-2150

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,8 +1,17 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_itemf = Cart_item.find(params[:id])
   end
 
   def update
+    @cart_itemf = Cart_item.find(params[:id])
+    @customer = current_customer(@customer)
+    if @itemf.update(cart_item_params)
+      flash[:notice] ="Cart_item updated successfully."
+      redirect_to public_cart_items_path
+    else
+      reder :index
+    end
   end
 
   def destroy
@@ -12,6 +21,14 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    @cart_item = Cart_item.new(cart_item_params)
+    @customer = current_customer
+    if @cart_item.save
+      flash[:notice] ="Cart_item was successfully created."
+      redirect_to public_cart_items_path(@customer)
+    else
+      redirect_to root_path
+    end
   end
 
   private
