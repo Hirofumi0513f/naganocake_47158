529a0c0338670f8145a6503534bb983ffd61efc7
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2023-01-20 13:30:02 +0000

    [Update]naganocake_2023-0120-2230

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
 class ApplicationController < ActionController::Base
+  # CSRF対策（クロスサイトリクエストフォージェリ）のための記述
+  protect_form_forgery with: :exception
+
   before_action :configure_permitted_parameters,if: :devise_controller?
+
+# customerがログインしている場合、紐づいたcart_itemを参照させる記述？
+helper_method :current_cart_item
+
+def current_cart_item
+  if current_customer
+    # customerとcart_itemの紐付け
+    current_cart_item = current_customer.cart_item || current_customer.create_cart_item!
+  else
+    # sessionとcart_itemの紐付け
+    current_cart_item = CartItem.find_by(id: session[:cart_id]) || CartItem.create_cart_item
+    session[:cart_id] ||= current_cart_item.id
+  end
+  current_cart_item
+end
 
   private
   # devise版ストロングパラメータ
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,9 @@
 class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
+
+  # カート内の商品合計に利用
+  def sum_of_price
+    item.price * quantity
+  end
 end
