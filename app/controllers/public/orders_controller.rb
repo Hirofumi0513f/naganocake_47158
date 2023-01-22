66ecb5132b83889fc34d15584ec7fd93559dfb2f
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2023-01-22 08:14:44 +0000

    [Update]naganocake_2023-0122-1714

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,17 +2,38 @@ class Public::OrdersController < ApplicationController
   before_action :authenticate_customer!
 
   def new
-    @orders = current_customer.orders
-    @order = @orders.new
-  end
-
-  def confirm
-  end
-
-  def complete
+    @order = Order.new
   end
 
   def create
+    # ログインしているユーザに紐づく、カートアイテムに入れる
+    cart_items = current_customer.cart_items
+    # ログインしているユーザのオーダー作成
+    @order = current_customer.orders.new(order_params)
+
+    if @order.save
+      cart_items.each do |cart|
+        order_item = OrderItem.new
+        order_item.item_id = cart.item_id
+        order_item.order_id = @order.id
+        order_item.order_price = cart.item.price
+        order_item.save
+      end
+      redirect_to public_confilm_path
+      # ユーザに関連するカートのデータ（購入データ）を全て削除（カートを空にする）
+      cart_items.destroy_all
+    else
+      @order = Order.new(order_params)
+      render:new
+    end
+  end
+
+  #注文確認画面のアクション
+  def confirm
+  end
+
+  # 注文完了画面のアクション
+  def complete
   end
 
   def index
@@ -22,5 +43,11 @@ class Public::OrdersController < ApplicationController
   end
 
   private
+  def order_params
+    params.require(:order).permit(:name, :address, :price)
+  end
 
+  def address_params
+    params.require(:order).permit(:name, :address)
+  end
 end
