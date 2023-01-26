deee5ebbe5183d5abdee9753f4829fd00cb715e5
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2023-01-26 12:42:07 +0000

    [Update]naganocake_2023-0126-2142

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,14 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @orderf = Order.find(params[:id])
+    @total_amount = 0
   end
 
   def update
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:customer_id, :postal_code, :address, :name, :postage, :payment_method, :status, :price)
   end
 end
