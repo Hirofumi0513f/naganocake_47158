b61eb70c4d20a36cd75b4a1e0aa0be9c649fda8e
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2023-01-26 12:22:44 +0000

    [Update]naganocake_2023-0126-2122

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -3,6 +3,9 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
 
   def top
-    @orders = Order.all
+    # .page(params[:page]).per(10):ページネーションで１ページあたり10件表示させる
+    @orders = Order.all.order(created_at: :desc).page(params[:page]).per(10)
+
+    @total_amount = 0
   end
 end
--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,4 +1,12 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
+    @order_detailsf = OrderDetails.find(params[:order_id])
+
+    @total_amount = 0
+  end
+
+    private
+  def order_details_params
+    params.require(:order_details).permit(:order_id, :item_id, :amount, :making_status, :price)
   end
 end
