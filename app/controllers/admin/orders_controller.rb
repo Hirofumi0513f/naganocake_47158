c02fc9c94cfca32c0b68061523461e7b5e12178a
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2023-01-26 23:44:06 +0000

    [Update]naganocake_2023-0127-0844

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,9 +2,19 @@ class Admin::OrdersController < ApplicationController
   def show
     @orderf = Order.find(params[:id])
     @total_amount = 0
+
+    # .page(params[:page]).per(10):ページネーションで１ページあたり10件表示させる
+    @orders = Order.all.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def update
+    @orderf = Order.find(params[:id])
+    if @orderf.update(order_params)
+      flash[:notice] ="Order updated successfully."
+      redirect_to admin_order_path(order.id)
+    else
+      reder :show
+    end
   end
 
   private
